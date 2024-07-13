9faf79e1a60ad893eb26e2b1a63c921914e892e7
Matt Watson <mwatson@renci.org>
2024-07-13 13:21:49 -0400

    tell webpack about mdx files

--- webpack.config.js
+++ webpack.config.js
@@ -68,6 +68,13 @@ module.exports = {
         }
       },
       {
+        test: /\.mdx$/,
+        use: [
+          { loader: 'babel-loader', options: {} },
+          { loader: '@mdx-js/loader', options: {} },
+        ]
+      },
+      {
         test: /\.md$/,
         use: [
           {
