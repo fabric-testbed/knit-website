586cecc7891902d9035da6afcb931f3f79cdf019
suejinkim20 <kim.suejin.20@gmail.com>
2024-08-19 15:43:09 -0400

    add agenda iframe

--- src/views/agenda.js
+++ src/views/agenda.js
@@ -6,6 +6,25 @@ export const AgendaView = () => {
       title="Agenda"
       description="Agenda"
     >
+      <div style={{
+        width: '100%',
+        height: '1150px',
+        position: 'relative',
+        overflow: 'hidden',
+      }}>
+        <iframe 
+          src="https://docs.google.com/document/d/1PW4NBZxc3GqD8ScMWWSixq8TJSoETfG1RDStaHAitG8/pub?embedded=true"
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        ></iframe>
+      </div>
     </Page>
   )
 }
