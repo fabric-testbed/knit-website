80f567bcfd241ac6ecb9175d65729b5c4d3ab22b
Yaxue Guo <yaxue.guo23@gmail.com>
2025-01-31 18:06:54 -0500

    #51: added workshop materials to the homepage

--- src/components/layout/banner.js
+++ src/components/layout/banner.js
@@ -12,30 +12,33 @@ const checkNotExpired = (start, end) => {
 }
 
 export const Banner = () => {
-  const bannerContent = {
-    "content": "FABRIC invites nominations for four awards recognizing innovative uses of FABRIC resources—Best Published Paper, Best FABRIC Matrix, Best FABRIC Experiment, and Best Classroom Use of FABRIC — submissions due by <b>Monday, February 24 at 11:59 PM ET</b>, and winners announced at KNIT10.",
-    "end_date": "2025-02-25 00:00:00+00:00",
-    "link": "https://docs.google.com/forms/d/e/1FAIpQLSeTp3i2iDhB7bHgN8ryMxZci8ya87yjeQd7_JMZImUodNinVA/viewform",
-    "start_date": "2025-01-22 00:00:00+00:00",
-    "button": ">>> Submit Form"
-  }
+  const banners = [
+    {
+      "content": "FABRIC invites nominations for four awards recognizing innovative uses of FABRIC resources—Best Published Paper, Best FABRIC Matrix, Best FABRIC Experiment, and Best Classroom Use of FABRIC — submissions due by <b>Monday, February 24 at 11:59 PM ET</b>, and winners announced at KNIT10.",
+      "end_date": "2025-02-25 00:00:00+00:00",
+      "link": "https://docs.google.com/forms/d/e/1FAIpQLSeTp3i2iDhB7bHgN8ryMxZci8ya87yjeQd7_JMZImUodNinVA/viewform",
+      "start_date": "2025-01-22 00:00:00+00:00",
+      "button": ">>> Submit Form"
+    }
+  ]
 
   return (
-    checkNotExpired(bannerContent.start_date, bannerContent.end_date) &&
+    banners.map(((banner, index) => checkNotExpired(banner.start_date, banner.end_date) &&
     <MUIAlert
       color="warning"
       size="md"
       variant="soft"
+      key={`banner-${index}`}
     >
-      <span>{Parser(bannerContent.content)}
+      <span>{Parser(banner.content)}
       <Link 
-        href={bannerContent.link}
+        href={banner.link}
         target="_blank"
         rel="noreferrer"
         sx={{ marginLeft: '20px' }}
       >
-        {bannerContent.button}
+        {banner.button}
       </Link></span>
-    </MUIAlert>
+    </MUIAlert>))
   )
 }
