5c86b85526b2a2b33670c817f248bd80b0bb731d
yaxue1123 <yaxue.guo23@gmail.com>
2025-07-13 19:36:27 -0400

    removed KNIT 10 content and replaced with 'coming soon'.

--- src/views/agenda.js
+++ src/views/agenda.js
@@ -6,26 +6,7 @@ export const AgendaView = () => {
       title="Agenda"
       description="Agenda"
     >
-      <div style={{
-        width: '100%',
-        height: '3800px',
-        position: 'relative',
-        overflow: 'hidden',
-      }}>
-        <iframe 
-          src="https://docs.google.com/document/d/e/2PACX-1vS9cX7THv3GkdzV1DYYHeWgwa63NiiNowZKvI4z-knmn-CMaLIUWyhplQ1cCjMWreFR8C2ii4WdCX91/pub?embedded=true"
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            width: '100%',
-            height: '100%',
-            border: 'none',
-          }}
-        ></iframe>
-      </div>
+      <em>Agenda coming soon.</em>
     </Page>
   )
 }
