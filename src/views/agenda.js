d3a0b5b65188e64fff1bda7330e1a49b0b5aa608
yaxue1123 <yaxue.guo23@gmail.com>
2025-12-10 11:57:59 -0500

    #106: updated KNIT 12 agenda

--- src/views/agenda.js
+++ src/views/agenda.js
@@ -1,7 +1,15 @@
 export const AgendaView = () => {
   return (
-  <div style={{ width: "800px", margin: "2rem auto", color: "#0075A9", fontStyle: "italic" }}>
-    Information coming soon.
+  <div style={{ width: "800px", margin: "0 auto", overflowX: "auto" }}>
+      <iframe 
+          src="https://docs.google.com/document/d/e/2PACX-1vT8ID-itYPA1PH5rzg1bxD-5evDMVBmqC7xWYs9JWrKNVeRietj85JJqK6qYSDh6Bpc52IrbunzVZSw/pub?embedded=true"
+          style={{
+          width: "1100px",
+          height: "4000px",
+          border: "none",
+          marginLeft: "-15px"
+          }}
+      />
   </div>
   )
 }
