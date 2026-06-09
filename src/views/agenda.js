9af7f89a4f8694e58e92db6729656991601315d3
yaxue1123 <yaxue.guo23@gmail.com>
2026-06-09 16:41:02 -0400

    Move KNIT 12 to past workshops & add placeholder content for KNIT 13

--- src/views/agenda.js
+++ src/views/agenda.js
@@ -1,15 +1,12 @@
+import Agenda from '@content/agenda.mdx'
+
+import { Page } from '@components/layout'
+import { componentMap } from "@components/markdown"
+
 export const AgendaView = () => {
   return (
-  <div style={{ width: "800px", margin: "0 auto", overflowX: "auto" }}>
-      <iframe 
-          src="https://docs.google.com/document/d/e/2PACX-1vT8ID-itYPA1PH5rzg1bxD-5evDMVBmqC7xWYs9JWrKNVeRietj85JJqK6qYSDh6Bpc52IrbunzVZSw/pub?embedded=true"
-          style={{
-          width: "1100px",
-          height: "10550px",
-          border: "none",
-          marginLeft: "-15px"
-          }}
-      />
-  </div>
+    <Page title="Agenda" description="KNIT Workshop Agenda">
+      <Agenda components={componentMap} />
+    </Page>
   )
 }
---
+++ src/views/past-knits/knit12.js
@@ -0,0 +1,15 @@
+import KNIT12 from '@content/past-knits/knit12.mdx'
+
+import { Page } from '@components/layout'
+import { componentMap } from "@components/markdown"
+
+export const Knit12View = () => {
+  return (
+    <Page
+      title=""
+      description=""
+    >
+      <KNIT12 components={componentMap} />
+    </Page>
+  )
+}
